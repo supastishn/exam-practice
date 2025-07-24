e138f628e4590e875ef406df12ae654bb7abe05a
supastishn <omareeto2012@hotmail.com>
2025-07-24 21:12:50 +0000

    ci: add GitHub Actions workflow and configure Vite for GitHub Pages deploy

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './css/styles.css'
 import './css/themes.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
